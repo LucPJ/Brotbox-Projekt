ee0df27ee932845641a8be2953b173e4dbc417c2
Lucy Palencia <lucy.palencia@gmail.com>
2023-01-05 10:51:31 +0100

    Kategorien fetch

--- src/components/client.jsx
+++ src/components/client.jsx
@@ -1,6 +1,9 @@
 import * as contentful from 'contentful';
 
 export const client = contentful.createClient({
-    space: process.env.REACT_APP_CONTENTFUL_SPACE,
-    accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+    //space: process.env.REACT_APP_CONTENTFUL_SPACE,
+    //accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+
+    space: 'nam9u7ux6gyj',
+    accessToken: 'pZ6js2Dk3PNK9gdmmvdyJmcNybmBSDOnWp9NQFLskZA',
 });
